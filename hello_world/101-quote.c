6956cfb721fe6d8b007376097ca26cfc41d15e87
RedginaldGodeau <5657@holbertonstudents.com>
2022-09-30 09:33:36 -0700

    last

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,9 +7,9 @@
 */
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int i = 0;
-	for(i = 0; i > sizeof(sentence); i++) {
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long unsigned int i = 0;
+	for(; i < sizeof(sentence); i++) {
 		putchar(sentence[i]);
 	}
 	return (1);
