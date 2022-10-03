d79992f76b841dd984ac328911ce0995e35f4c03
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-03 06:52:28 -0700

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char list = 'a';
+	for(int i = 0; list[i] != z; i++ )
+		putchar (list);
+	putchat (list);
+
+	return (0);
+}
