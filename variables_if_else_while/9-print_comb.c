cadbe60a3d73ab70ab9236ca9fc20f478787793f
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-03 08:23:39 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n = '0;
+	
+	for ( ; n <= '9'; n++)
+	{
+		putchar(n);
+
+		if (n != '0' && n != '9')
+		{
+			putchar(',');
+		}
+
+		putchar(',');
+	}
+
+	return (0);
+}
