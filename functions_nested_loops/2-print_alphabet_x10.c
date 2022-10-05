58187b9777857c02733ac004ff49367781f01564
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-05 02:12:49 -0700

    adding task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10()
+{
+	char a;
+	int i = 0;
+
+	for(; i < 10; i++)
+	{
+		for (a = 'a'; a >= 'z'; a++)
+			_putchar(a);
+		_putchar('\n');
+	}
+}
