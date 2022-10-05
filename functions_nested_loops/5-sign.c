f7dc229d604c093746db27742a99e0d5ffd1f626
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-05 02:53:48 -0700

    adding 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,17 @@
 #include "main.h"
 
-int print_sign(int n) {
-	if (n > 0) return (1);
-	else if (n < 0) return (-1);
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
 	return (0);
 }
