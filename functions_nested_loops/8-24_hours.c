a4d3ada04c06d3ef1bc76afefddf1fd4736b54e8
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-05 04:31:32 -0700

    adding files

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 void jack_bauer(void) {
-	int h,m = {0};
+	int h,m;
 
-	for (; h < 24 ; h++)
-		for (; m < 60; m++)
-			printf("%0d:%0d",h,m);
+        for (h = 0; h < 24 ; h++)
+                for (m = 0; m < 60; m++)
+                        printf("%0d:%0d\n",h,m);	
 }
 
