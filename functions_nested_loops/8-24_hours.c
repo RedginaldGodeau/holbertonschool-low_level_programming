8500c88ba16d9fca8eca9e49ea8f34df2ec8e988
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-05 04:33:55 -0700

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,6 +6,6 @@ void jack_bauer(void) {
 
         for (h = 0; h < 24 ; h++)
                 for (m = 0; m < 60; m++)
-                        printf("%0d:%0d\n",h,m);	
+                        printf("%02d:%02d\n",h,m);	
 }
 
