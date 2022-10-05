93f9ca565c80331566f482650a14f92b5d1e8702
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-05 00:57:19 -0700

    adding task 1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char put[] = "_putchar";
+	int i = 0;
+	for (; put[i] != '\0'; i++)
+		_putchar(put[i]);
+
+	_putchar('\n');
+	return (0);
+}
