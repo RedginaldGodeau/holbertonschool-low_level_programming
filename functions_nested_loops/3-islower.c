c9359ecd7ac370f1cabb8b0b1d5dc9dc7f5744ca
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-06 04:58:28 -0700

    update

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+* _islower - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @c: int parmameter
+*/
+
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z' ? 1 : 0);
