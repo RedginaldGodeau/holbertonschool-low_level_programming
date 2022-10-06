65a54e7c1b1fffcfb11fe0c19e2e85eb215e90a1
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-06 05:55:30 -0700

    comcom

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,33 +4,33 @@
 * times_table - Entry point...
 * Description: 'the program desc'
 * Return: Always 0 (Success)
-* @c: int parmameter
 */
 
 void times_table(void)
 {
 	int i, i2;
-        for (i = 0; i <= 9; i++)
-        {
-                for(i2 = 0; i2 <= 9; i2++)
-                {
-                        int num = i * i2;
-                        int unit = num % 10;
-                        int dix = (num - unit) / 10;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (i2 = 0; i2 <= 9; i2++)
+		{
+			int num = i * i2;
+			int un = num % 10;
+			int dix = dix - un;
+
 			dix = (dix == 0 ? ' ' : dix + '0');
 
-                        _putchar(dix);
-			_putchar('0' + unit);
-                        if(i2 != 9)
-                        {
-                       	 _putchar(',');
-			
-			if(i2 != 0)
-                       		_putchar(' ');
-                        }
+			if (i2 != 0)
+				_putchar('0' + un);
 
-                }
-            _putchar('\n');
-          }
+			if (i2 != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
 }
 
