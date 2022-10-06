a0a1a3287184f91ced2a767dd58ab078a244e374
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-06 06:46:17 -0700

    add 7

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_last_digit - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @a: int parmameter
+* @b: int parmameter
+*/
+
+int add(int a, int b) 
+{
+	return (a + b);
+}
