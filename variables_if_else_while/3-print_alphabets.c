f82c2a7684f7d33f43d4eb0f6827ffc22d82ba61
Soundwave95 <redginaldgodeau95@gmail.com>
2022-10-08 17:30:27 +0200

    edit 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,16 +4,19 @@
 * main - Entry point
 *
 * Return: Always 0 (Success)
+*
 */
+
 int main(void)
 {
 	char list = 'a';
 
-	for ( ; list < 'z' + 1; list++)
-		putchar (list);
-	for (list = 'A' ; list < 'Z' + 1; list++)
-                putchar (list);
-	putchar ('\n');
+	for(; list <= 'z'; list++)
+		putchar(list);
 
+	for(list = 'A'; list <= 'Z'; list++)
+		putchar(list);
+
+	putchar('\n');
 	return (0);
 }
