c56aa1c2ef73854a97671307283c913f7ade2f9f
Soundwave95 <redginaldgodeau95@gmail.com>
2022-10-08 14:51:23 +0200

    proj 4

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @n: int parmameter
+*/
+
+int _isupper(char c)
+{
+    return (c >= 'A' && c <= 'Z' ? 1 : 0);
+}
