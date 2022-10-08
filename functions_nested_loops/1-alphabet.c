54adb99d6ef13a23d80f642978ac16617daba538
Soundwave95 <redginaldgodeau95@gmail.com>
2022-10-08 17:47:46 +0200

    edit 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * alphabet - check the code
- *
- * Return: Always 0.
- */
+* print_alphabet - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+*/
 
 void print_alphabet(void)
 {
