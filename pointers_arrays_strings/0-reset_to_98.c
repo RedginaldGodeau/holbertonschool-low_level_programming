9419027782a4fe1bf8287ccd71859aba9d112b47
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-10 04:47:49 -0700

    adding pointers files

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.c"
+
+/**
+* times_table - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @n: pointer
+*/
+
+void reset_to_98 ( int *n)
+{
+	*n = 0;
+}
