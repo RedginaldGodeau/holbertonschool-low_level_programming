0ac6c1263cd65d745ccef50121c0fb3df7b2ca7c
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-11 11:41:47 -0700

    correcting t1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* times_table - Entry point...
+* reset_to_98 - Entry point...
 * Description: 'the program desc'
 * Return: Always 0 (Success)
 * @n: pointer
 */
 
-void reset_to_98 ( int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
