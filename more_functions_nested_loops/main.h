fee955d2c245b19f9779abb2e3d5119d1929c8d6
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-14 04:26:57 -0700

    Update main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -24,4 +24,12 @@ void print_diagonal(int);
 void print_square(int);
 void print_triangle(int);
 
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 #endif
