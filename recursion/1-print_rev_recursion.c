7d2357aa5ccc208f11b24b4bd00b1bf8be925cef
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-20 02:04:07 -0700

    corr

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* _puts_recursion - Entry point...
+* _print_rev_recursion- Entry point...
 * Description: 'the program desc'
 * Return: Always 0 (Success)
 * @s: pointer parmameter
 */
 
-void _puts_recursion (char *s)
+void _print_rev_recursion (char *s)
 {
 	if (*s != '\0')
 	{
