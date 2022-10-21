884c6c493b5817d6a07660b323ff6d0684d93d56
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-21 12:56:19 -0700

    edit

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -19,9 +19,11 @@ int square(int low, int high, int n)
 	if (low <= high)
 	{
 		int mid = (low + high) / 2;
-		if ((mid * mid <= n) && (mid + 1) * (mid + 1) > n)
+		if ((mid * mid <= n) && ((mid + 1) * (mid + 1) > n))
 			return mid;
 		else if (mid * mid < n)
+			return (square(low, mid + 1, n));
+		else
 			return (square(low, mid - 1, n));
 	}
 	return (low);
