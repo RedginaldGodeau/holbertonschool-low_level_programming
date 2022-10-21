247abc3960e3d92391901d656d62b8869ab21459
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-21 13:17:09 -0700

    edit

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,11 +6,14 @@
 * @s: pointer parmameter
 */
 
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
 	putchar(*s);
 	if (*s++ != '\0')
 		_puts_recursion(s);
-	else
-		putchar('\n');
 }
