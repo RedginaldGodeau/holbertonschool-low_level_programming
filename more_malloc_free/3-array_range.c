a76fc2aabd4b8f2a725c30216afb621a91099642
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-28 09:54:57 -0700

    edi

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,13 +15,16 @@ void *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * (max - min));
+	ptr = malloc(sizeof(int) * (max - min) + 1);
 
 	if (ptr == NULL)
 		return (NULL);
 
 	for (; i < max; i++)
 		ptr[i - min] = i;
+
+	if (min == max)
+		ptr[0] = min;
 
 	return (ptr);
 }
