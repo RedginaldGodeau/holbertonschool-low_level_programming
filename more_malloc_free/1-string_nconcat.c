dbf0ae749bde1a665c7ab6d4084b850c932d95db
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-28 11:38:39 -0700

    corr

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int len1, len2;
+	int len1, len2, max;
 	int i = 0;
 
 	for (len1 = 0; s1 != NULL && s1[len1] != '\0'; len1++)
@@ -19,16 +19,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len2 = 0; s2 != NULL && s2[len2] != '\0'; len2++)
 		;
 
-	ptr = malloc(n + len1 + 1);
+	max = (n < len2) ? n : len1;
+	ptr = malloc(max + len1 + 1);
+
 	if (ptr == NULL)
 		return (NULL);
 
 	for (; s1 != NULL && i < len1; i++)
 		ptr[i] = s1[i];
 
-	for (i = 0; s2 != NULL && i < len2 && i < (int) n; i++)
+	for (i = 0; s2 != NULL && i < max; i++)
 		ptr[len1 + i] = s2[i];
-	ptr[len1 + n] = '\0';
+	ptr[len1 + max] = '\0';
 
 	return (ptr);
 }
