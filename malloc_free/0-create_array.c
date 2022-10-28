c1779b18268193147e3aff479f668e1bd7244127
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-28 08:28:56 -0700

    mem

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(char) * size + 1);
+	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
 
