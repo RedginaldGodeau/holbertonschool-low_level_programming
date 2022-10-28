66f517ddd122b7cbd5def4404b228ccfa8d8dc77
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-28 09:27:35 -0700

    push

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+* _calloc - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @nmemb: int parmameter
+* @size: int parameter
+*/
+
+char *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	int i = 0;
+
+	if (nmemb * sizeof(size) == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * sizeof(size));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (; i < size * nmemb; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
