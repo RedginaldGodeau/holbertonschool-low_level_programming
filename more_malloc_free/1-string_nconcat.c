3e81f05f1e7d52ee9131c66b5a084e2058c71b28
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-28 08:53:17 -0700

    ed

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (; i < len1; i++)
 		ptr[i] = s1[i];
 
-	for (i = 0; i < len2 && i < n; i++)
+	for (i = 0; i < len2 && i < (int) n; i++)
 		ptr[len1 + i] = s2[i];
 	ptr[len1 + n] = '\0';
 
