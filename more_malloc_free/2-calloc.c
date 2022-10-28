31d9c848e1419e80ebd379e3999da65974069147
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-28 09:29:47 -0700

    edi

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ char *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	int i = 0;
 
-	if (nmemb * sizeof(size) == 0)
+	if (nmemb * size == 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * sizeof(size));
