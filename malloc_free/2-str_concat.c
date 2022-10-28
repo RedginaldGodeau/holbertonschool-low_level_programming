7a864eea8ff26ce4ff4540ba098f65097e75bf49
RedginaldGodeau <5657@holbertonstudents.com>
2022-10-28 08:21:17 -0700

    edit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,14 +15,17 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i, i2;
 
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-
+	{
+		ptr = malloc(sizeof(char));
+		ptr[0] = '\0';
+		return (ptr);
+	}
 	for (len1 = 0; s1 != NULL && s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2 != NULL && s2[len2] != '\0'; len2++)
 		;
 
-	ptr = malloc(sizeof(char) * (len1 + len2));
+	ptr = malloc(sizeof(char) * (len1 + len2) + 1);
 	if (ptr == NULL)
 		return (NULL);
 
