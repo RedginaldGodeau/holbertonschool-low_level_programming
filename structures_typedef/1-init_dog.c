cc7891293f8da90b9dc962084b1e7e40ab404b37
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-02 11:54:47 -0700

    Corr

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,20 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
 * init_dog - Entry point...
 * Description: 'the program desc'
 * Return: Always 0 (Success)
-* @d: struct parmameter
-* @name: pointer parmameter
+* @owner: pointer parmameter
 * @age: float parameter
-* @owner: pointer parameter
+* @d: pointer paramater
+* @name: pointer paramater
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (!d)
-		return;
-
 	d->name = name;
 	d->owner = owner;
 	d->age = age;
