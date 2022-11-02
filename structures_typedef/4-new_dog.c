2c0e1b3df71ffec0169cad8fd94a288061b94f22
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-02 12:40:27 -0700

    edit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,16 +1,44 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
-* free_dog - Entry point...
+* new_dog - Entry point...
 * Description: 'the program desc'
 * Return: Always 0 (Success)
-* @d: struct parmameter
+* @name: pointer parmameter
+* @age: float parameter
+* @owner: char parameter
 */
 
-void free_dog(struct dog *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *copy;
+
 	if (!d)
-		return;
+		return (NULL);
+
+	copy = malloc(sizeof(dog_t));
+	if (!copy)
+	{
+		free(copy->name);
+		free(copy->owner);
+		free(copy);
+		return (NULL);
+	}
+
+	copy->name = malloc(strlen(name));
+	copy->owner = malloc(strlen(owner));
+
+	if (!copy->name || !copy->owner)
+	{
+		free(copy->name);
+		free(copy->owner);
+		free(copy);
+	}
+
+	strcpy(copy->name, name);
+	strcpy(copy->owner, owner);
+	copy->age = age;
 
 	free(d);
+	return (copy);
 }
