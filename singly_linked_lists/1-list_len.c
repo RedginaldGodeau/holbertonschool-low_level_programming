e938053bc4979efdbee541f4c2635da68ef600e3
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-15 05:33:05 -0800

    adding task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include "main.h"
+
+/**
+* list_len - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @h: list_t pointer parmameter
+*/
+
+size_t list_len(const list_t *h)
+{
+	if (h->next)
+		return (1 + (list_len((const list_t *) h->next)));
+	else
+		return (1);
+}
