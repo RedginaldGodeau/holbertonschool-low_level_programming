a407294bf1f1adca520d4eaf4969dbee18ba8b31
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 14:04:06 -0800

    corr

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	int i = 0;
 	listint_t *tmp;
 
-	if (!head || !(*head))
+	if (!head || !(*head) || (int) index > 0)
 		return (-1);
 
 	for (; i < (int) index - 1; i++)
