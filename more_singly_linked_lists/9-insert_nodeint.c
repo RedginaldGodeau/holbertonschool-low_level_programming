6f6aa1918ece51183b6bc951edbb18ed3360e71f
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 13:44:34 -0800

    corr end

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (; i <= (int) idx; i++)
 	{
-		if (!(*head)->next)
+		if (!(*head))
 			return (NULL);
 
 		(*head) = (*head)->next;
