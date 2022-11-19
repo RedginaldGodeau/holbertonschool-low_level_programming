623e05af27bef0eaab39b259b9aa770904db5b52
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 14:00:54 -0800

    add 10t

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+* delete_nodeint_at_index - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @head: list parmameter
+* @index: index parameter
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	int i = 0;
+	listint_t *tmp;
+
+	if (!head || !(*head))
+		return (-1);
+
+	for (; i < index - 1; i++)
+	{
+		if (!(*head)->next)
+			return (-1);
+		(*head) = (*head)->next;
+	}
+	tmp = (*head);
+	(*head)->next = (*head)->next->next;
+	free(tmp);
+	return (1);
+}
