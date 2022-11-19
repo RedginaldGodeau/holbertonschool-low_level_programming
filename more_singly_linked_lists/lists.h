29c48dc79cbcc9787cf33dfeaff92e5a3da99f73
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 13:00:39 -0800

    adding const

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -21,5 +21,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
+int sum_listint(const listint_t *head);
 #endif
