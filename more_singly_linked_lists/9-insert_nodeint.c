fccf0f669c41f54c6344bbfad2235279dadf2405
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 13:22:31 -0800

    corr function to get index

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (!(*head)->next)
 			return (NULL);
-		(*head) = head->next;
+		(*head) = (*head)->next;
 	}
 	tmp = (*head)->next;
 
