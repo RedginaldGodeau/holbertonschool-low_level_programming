c2bcd3902e5574355d95aa40ce459ebe24107e64
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 11:59:50 -0800

    remove str on file

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,11 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new;
 	listint_t *next;
 
-	if (!str)
-		return (NULL);
-
 	new = malloc(sizeof(listint_t));
-
 	if (!new)
 		return (NULL);
 
