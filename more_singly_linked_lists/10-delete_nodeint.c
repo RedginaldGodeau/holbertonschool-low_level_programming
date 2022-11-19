009f5038855b46d306ff8e90615cd9141a25e48b
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 14:05:51 -0800

    test

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	int i = 0;
-	listint_t *tmp;
+	/*listint_t *tmp;*/
 
 	if (!head || !(*head) || (int) index > 0)
 		return (-1);
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			return (-1);
 		(*head) = (*head)->next;
 	}
-	tmp = (*head);
+	/*tmp = (*head);*/
 	(*head)->next = (*head)->next->next;
 	return (1);
 }
