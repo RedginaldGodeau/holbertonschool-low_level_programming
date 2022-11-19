5eb73bb9e395c9d87a84a77efdf98133db6b0eeb
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 12:05:40 -0800

    adding t6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+* pop_listint - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @head: list parmameter
+*/
+
+int pop_listint (listint_t **head)
+{
+	if (head && (*head))
+		return (0);
+
+	head = (*head);
+
+	while (head)
+		head = head->next;
+
+	free(head);
+	head = NULL;
+
+	return (a + b);
+}
