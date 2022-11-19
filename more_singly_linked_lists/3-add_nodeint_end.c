cd1691dc0a8c833e1220ad9ee817b23d5f0c4cfd
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-19 11:57:38 -0800

    Adding task 3

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* add_nodeint_end - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @head: struct parmameter
+* @n: string parmameter
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *next;
+
+	if (!str)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+
+	if (!(*head))
+	{
+		(*head) = new;
+		return (new);
+	}
+
+	next = malloc(sizeof(listint_t));
+	if (!next)
+		return (NULL);
+
+	next = (*head);
+	while (next->next != NULL)
+		next = next->next;
+
+	next->next = malloc(sizeof(listint_t));
+	next->next = new;
+
+	return (new);
+}
