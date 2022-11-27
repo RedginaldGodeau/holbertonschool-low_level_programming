e8520234870a0d7406abdccfaf863b1d2ab0e52d
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-27 21:30:25 +0100

    adding project

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+/**
+* read_textfile - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @filename: file parmameter
+* @letters: lenght parameter
+*/
+
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f = open(filename, O_RDONLY, 0);
+	char buffer;
+	ssize_t i = 0;
+
+	if (!f)
+		return (0);
+
+	while (read(f, &buffer, 1) > 0 && i < (int) letters)
+		i += (ssize_t) write(1, &buffer, 1);
+	
+	close(f);
+	return (i);
+
+}
---
+++ file_io/1-create_file.c
@@ -0,0 +1,28 @@
+/**
+* read_textfile - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @filename: file parmameter
+* @letters: lenght parameter
+*/
+
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+	int f, i = 0;
+
+	if (!filename)
+		return (-1);
+	
+	f = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 300);
+	if (!f)
+		return (-1);
+
+	for (; text_content[i] != '\0'; i++)
+		write(f, &text_content[i], 1);
+
+	close(f);
+	return (i);
+
+}
