6a0cedfacc72069710ab26125eb890913a0cea9b
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-27 21:54:23 +0100

    corr betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	
+
 	f = open(filename, O_WRONLY | O_TRUNC);
 	if (f == -1)
 		f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
