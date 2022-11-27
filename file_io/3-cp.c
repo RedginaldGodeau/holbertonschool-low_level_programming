a2d665e58db1edf386d1dcd42c5d36728adcf5cc
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-27 22:44:32 +0100

    adding if err

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,9 +8,9 @@
 
 #include "main.h"
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int fc,fp;
+	int fc, fp;
 	char buffer;
 
 	if (argc == 2)
@@ -27,19 +27,37 @@ int main (int argc, char *argv[])
 	fc = open(argv[1], O_RDONLY);
 	if (fc == -1)
 	{
-		printf("Error: Can't write to NAME_OF_THE_FILE");
-		return (99);
+		printf("Error: Can't read from file NAME_OF_THE_FILE");
+		return (98);
 	}
-	
+
 	fp = open(argv[2], O_WRONLY | O_TRUNC);
 	if (fp == -1)
 		fp = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	while (read(fc, &buffer, 1) > 0)
-		write(fp, &buffer, 1);
+	if (fp == -1)
+	{
+		printf("Error: Can't write to NAME_OF_THE_FILE");
+		return (98);
+	}
 
-	close(fc);
-	close(fp);
+	while (read(fc, &buffer, 1) > 0)
+		if (write(fp, &buffer, 1) == -1)
+		{
+			printf("Error: Can't write to NAME_OF_THE_FILE");
+			return (99);
+		}
+
+	if (close(fc) == -1)
+	{
+		printf("Error: Can't close fd FD_VALUE");
+		return (100);
+	}
+	if (close(fp) == -1)
+	{
+		printf("Error: Can't close fd FD_VALUE");
+		return (100);
+	}
 
 	return (0);
 }
