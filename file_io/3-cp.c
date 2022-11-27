07af563e212a9c1f595feed2424f47df16d4315a
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-27 23:46:56 +0100

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,12 +1,42 @@
 /**
-* create_file - Entry point...
+* print_err_str - Entry point...
 * Description: 'the program desc'
 * Return: Always 0 (Success)
-* @filename: file parmameter
-* @text_content: text in file parameter
+* @err: file parmameter
+* @str: text in file parameter
+* @value: str parameter
 */
 
 #include "main.h"
+
+int print_err_str(int err, const *char str, const *char value)
+{
+	if (value)
+		dprintf(STDERR_FILENO, str, value);
+	else
+		dprintf(STDERR_FILENO, str);	
+	return (err);
+}
+
+/**
+* print_err_int - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+*/
+
+int print_err_int(int err, const *char str, int value)
+{
+	dprintf(STDERR_FILENO, str, value);
+	return (err);
+}
+
+/**
+* main - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @argv: files parmameter
+* @argv: int parameter
+*/
 
 int main(int argc, char *argv[])
 {
@@ -14,62 +44,38 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 	
 	if (argc == 2)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
-	}
+		return (print_err_str(97, "Usage: cp file_from file_to\n"));
 
 	if (!argv[1])
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
+		return (print_err_str(98, "Error: Can't read from file %s\n", argv[1]));
 
 	fc = open(argv[1], O_RDONLY);
 	if (fc == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
+		return (print_err_str(98, "Error: Can't read from file %s\n", argv[1]));
 
 	fp = open(argv[2], O_WRONLY | O_TRUNC);
 	if (fp == -1)
 		fp = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fp == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		return (99);
-	}
+		return (print_err_str(99, "Error: Can't write to %s\n", argv[2]));
 
 	status = 1;
 	while (status != 0)
 	{
 		status = read(fc, &buffer, 1024);
 		if (status == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			return (98);
-		}
+			return (print_err_str(98, "Error: Can't read from file %s\n", argv[1]));
 		if (write(fp, &buffer, status) == -1)
-		{
-			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
-			return (99);
-		}
+			return (print_err_str(99, "Error: Can't write to %s\n", argv[2]));
 	}
 
 	status = close(fc);
 	if (status == -1)
-	{
-		dprintf(STDERR_FILENO,"Error: Can't close fd %d", status);
-		return (100);
-	}
+		return (print_err_int(100, "Error: Can't close fd %d", status));
 	status = close(fp);
 	if (status == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", status);
-		return (100);
-	}
+		return (print_err_int(100, "Error: Can't close fd %d", status));
 
 	return (0);
 }
