f1e0a95316d6922aaf55b6b569436ec889fd7bff
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-27 21:35:42 +0100

    Correcting errors

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	
-	f = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 300);
+	f = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
 	if (!f)
 		return (-1);
 
@@ -23,6 +23,6 @@ int create_file(const char *filename, char *text_content)
 		write(f, &text_content[i], 1);
 
 	close(f);
-	return (i);
+	return (1);
 
 }
