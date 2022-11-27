6954d1320c5dd6657ea8c6d79f5d3cd25fb36bd1
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-27 22:53:12 +0100

    adding dprintf

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,19 +15,19 @@ int main(int argc, char *argv[])
 
 	if (argc == 2)
 	{
-		dprintf("Usage: cp file_from file_to");
+		printf("Usage: cp file_from file_to");
 		return (97);
 	}
 	if (!argv[1])
 	{
-		dprintf("Error: Can't read from file NAME_OF_THE_FILE");
+		printf("Error: Can't read from file NAME_OF_THE_FILE");
 		return (98);
 	}
 
 	fc = open(argv[1], O_RDONLY);
 	if (fc == -1)
 	{
-		dprintf("Error: Can't read from file NAME_OF_THE_FILE");
+		printf("Error: Can't read from file NAME_OF_THE_FILE");
 		return (98);
 	}
 
@@ -37,25 +37,25 @@ int main(int argc, char *argv[])
 
 	if (fp == -1)
 	{
-		dprintf("Error: Can't write to NAME_OF_THE_FILE");
+		printf("Error: Can't write to NAME_OF_THE_FILE");
 		return (98);
 	}
 	
 	while (read(fc, &buffer, 1) > 0)
 		if (write(fp, &buffer, 1) == -1)
 		{
-			dprintf("Error: Can't write to NAME_OF_THE_FILE");
+			dprintf(fp,"Error: Can't write to NAME_OF_THE_FILE");
 			return (99);
 		}
 
 	if (close(fc) == -1)
 	{
-		dprintf("Error: Can't close fd FD_VALUE");
+		dprintf(fc,"Error: Can't close fd FD_VALUE");
 		return (100);
 	}
 	if (close(fp) == -1)
 	{
-		dprintf("Error: Can't close fd FD_VALUE");
+		dprintf(fp,"Error: Can't close fd FD_VALUE");
 		return (100);
 	}
 
