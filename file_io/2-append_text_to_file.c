d229e66043510bd37d66f0146baf466d2d6a38a1
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-27 22:15:04 +0100

    corr err file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	f = open(filename, O_RDWR | O_APPEND);
-	if (!f == -1)
+	if (f == -1)
 		return (-1);
 
 	if (text_content)
