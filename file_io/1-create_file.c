1e8e20da8f4d0e44301ee2c6ec59b32e256fe1bd
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-27 21:51:48 +0100

    file err

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,9 +16,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	
 	f = open(filename, O_TRUNC);
-	if (!f)
+	if (f == -1)
 		f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (!f)
+	if (f == -1)
 		return (-1);
 
 	if (text_content)
