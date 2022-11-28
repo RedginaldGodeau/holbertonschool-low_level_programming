336569c1b3f3deb46d2be724e3f46db1faf1f839
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-28 10:57:22 -0800

    Update to the task with new logic

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f = open(filename, O_RDONLY, 0), rd = -1;
+	int f = open(filename, O_RDONLY), rd = -1;
 	size_t length = 0;
 	char buffer[1024];
 
