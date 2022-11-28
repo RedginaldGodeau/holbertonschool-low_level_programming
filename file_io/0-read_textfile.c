8f10aa05101e6716e472029c2ce6dcfbe90e7b27
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-28 09:25:12 -0800

    correcting

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer[letters + 1] = '\0';
 
 	err = read(f, buffer, letters);
-	if (err < letters)
+	if (err < (int) letters)
 	{
 		dprintf(STDERR_FILENO, "%s", buffer);
 		return (0);
