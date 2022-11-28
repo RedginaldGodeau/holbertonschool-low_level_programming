84eec3ddf9a7e94c70dfa86f103879cca2d50eec
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-28 10:48:20 -0800

    Update to the task with new logic

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,36 +1,33 @@
 /**
-* read_textfile - Entry point...
-* Description: 'the program desc'
-* Return: Always 0 (Success)
-* @filename: file parmameter
-* @letters: lenght parameter
-*/
+ * read_textfile - Entry point...
+ * Description: 'the program desc'
+ * Return: Always 0 (Success)
+ * @filename: file parmameter
+ * @letters: lenght parameter
+ */
 
 #include "main.h"
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f = open(filename, O_RDONLY, 0), err;
-	char *buffer;
+	int f = open(filename, O_RDONLY, 0);
+	ssize_t length = 0, rd = -1;
+	char buffer[1024];
 
-	if (!f)
+	if (f == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters + 1);
-	buffer[letters + 1] = '\0';
-
-	err = read(f, buffer, letters);
-	if (err == 1685)
+	while (rd != 0)
 	{
-		dprintf(STDERR_FILENO, "%s", buffer);
-		close(f);
-		return (err);
+		rd = read(f, &buffer, letters);
+		if (rd == -1)
+		{
+			close(f);
+			return (length);
+		}
+		length += write(STDOUT_FILENO, &buffer, rd);
 	}
-	err = 0;
-
-	while (buffer[err] != '\0')
-		err += write(1, &buffer[err], 1);
 
 	close(f);
-	return ((ssize_t) err);
+	return (length);
 }
