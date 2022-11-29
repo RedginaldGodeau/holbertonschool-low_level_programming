9560b7f4a9542a2ed1e4727396bc4a9c117ab888
RedginaldGodeau <5657@holbertonstudents.com>
2022-11-29 08:39:26 -0800

    corr header

--- file_io/main.h
+++ file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,3 +11,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+#endif
