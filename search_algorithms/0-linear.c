58666c8fe5e933027c4bfa114cceb925c2806664
RedginaldGodeau <5657@holbertonstudents.com>
2023-01-09 04:48:09 -0800

    adding project

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+/**
+ * linear_search - search item in array...
+ * Description: 'search value in array'
+ * Return: Int
+ * @array: pointer array
+ * @size: size array
+ * @value: value of search
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t pos = 0;
+
+	for (; pos < size && array != value; pos++)
+		;
+
+	if (array[pos] != value)
+		return (-1);
+
+	return (pos);
+}
