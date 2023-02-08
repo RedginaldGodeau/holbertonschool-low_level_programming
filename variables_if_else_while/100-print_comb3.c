8303e9d54ce882634896efefe04d20268f14ee14
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-08 07:04:46 -0800

    adding task

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int i2 = 0;
+
+	for (; i < 9; i++)
+	{
+		for (i2 = i + 1; i2 < 10; i2++)
+		{
+			putchar('0' + i);
+			putchar('0' + i2);
+
+			if (i == 8 && i2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
